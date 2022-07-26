b71dbd0e467f1d170788ca614ba1824e059f7c63
Andrea Martinez <andrea.maort@gmail.com>
2022-07-26 21:16:54 +0200

    added word list and random word selection

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import Hangman from "./components/Hangman/Hangman";
 import Letters from "./components/Letters/Letters";
 import Result from "./components/Result/Result";
 import UsedLetters from "./components/UsedLetters/UsedLetters";
+import wordList from "./components/utilities/wordList";
 
 function App() {
+  const randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+  const letters = randomWord
+    .normalize("NFD")
+    .replace(/([aeio])\u0301|(u)[\u0301\u0308]/gi, "$1$2")
+    .normalize()
+    .split("");
+
   return (
     <>
       <div className="container">
